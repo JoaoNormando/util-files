ca2282f59b19e74474dac74f899ee7891edfb4f4
Joao Vitor Normando <joaonormando50@gmail.com>
2021-06-12 17:03:12 -0300

    added --forced in docker-delete and fix .aliases

--- bin/docker-delete-untagged-images.sh
+++ bin/docker-delete-untagged-images.sh
@@ -2,6 +2,6 @@
 
 echo "removing untagged images"
 
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc) --force
 
 echo "success"
